997d591efcf5705a486db0c3c068d4df8cff4fb7
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-07-24 11:12:48 +0530

    update user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -93,6 +93,7 @@ export const login = async (req, res) => {
       })
       .json({
         message: `Welcome back ${user.fullname}`,
+        user,
         success: true,
       });
   } catch (error) {
